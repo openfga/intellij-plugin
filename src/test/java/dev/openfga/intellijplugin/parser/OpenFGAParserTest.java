72e99b01e2458d080eb4221fdb2b746bed532129
Daniel Jeffery <daniel.jeffery@okta.com>
2024-08-27 16:54:39 -0700

    chore: add parser tests

--- src/test/java/dev/openfga/intellijplugin/parser/OpenFGAParserTest.java
+++ src/test/java/dev/openfga/intellijplugin/parser/OpenFGAParserTest.java
@@ -14,25 +14,169 @@ public class OpenFGAParserTest extends ParsingTestCase {
         return "src/test/testData";
     }
 
-    public void testschema() throws Throwable {
+    // Valid models
+
+    public void testSchema() throws Throwable {
         doCodeTest("""
                  model
                    schema 1.1
                  """);
     }
 
-    public void testnoschema() throws Throwable {
-        doCodeTest("""
-                 model
-                 """);
-    }
-
-    public void testuser() throws Throwable {
+    public void testUser() throws Throwable {
         doCodeTest("""
                  model
                    schema 1.1
                  type user
                  """);
     }
+
+    public void testMultipleTypes() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                 type user
+                 type admin
+                 type folder
+                 """);
+    }
+
+    public void testSpacing() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                 
+                 type user
+                 
+                 type admin
+                 
+                 type folder
+                 """);
+    }
+
+    public void testRelation() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                 
+                 type user
+                    relations
+                        define admin: [user]
+                 """);
+    }
+
+    public void testComments() throws Throwable {
+        doCodeTest("""
+                 # This is a sample model
+                 model
+                   schema 1.1
+                 # This is a comment that should be allowed
+                 type user
+                   relations
+                     # This is a comment that should be allowed
+                     define admin: [user]
+                 """);
+    }
+
+    // Invalid models
+
+    public void testNoSchemaInvalid() throws Throwable {
+        doCodeTest("""
+                 model
+                 """);
+    }
+
+    public void testSchemaInvalid() throws Throwable {
+        doCodeTest("""
+                model
+                  schema a.b
+                """);
+    }
+
+    public void testMissingRelationsInvalid() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                 
+                 type user
+                    define admin: [user]
+                 """);
+    }
+
+    public void testMixedOperatorsInvalid() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                  type user
+                    relations
+                      define own: [user]
+                      define follower: [user]
+                      define blocked: [user]
+                      define viewer: [user] and follower or own but not blocked
+                 """);
+    }
+
+    public void testMixedOperatorsWithParensInvalid() throws Throwable {
+        doCodeTest("""
+                 model
+                   schema 1.1
+                  type user
+                    relations
+                      define own: [user]
+                      define follower: [user]
+                      define blocked: [user]
+                      define viewer: [user] and (follower or own) but not blocked
+                 """);
+    }
+
+    public void testMixedOperatorsWithMixedParensInvalid() throws Throwable {
+        doCodeTest("""
+                model
+                  schema 1.1
+                type user
+                  relations
+                    define follower: [user]
+                    define blocked: [user]
+                    define restricted: [user]
+                    define viewer: [user] and (follower but not blocked but not restricted)
+                """);
+    }
+
+    public void testMixOfVersionsShouldInvalid() throws Throwable {
+        doCodeTest("""
+                model
+                  schema 1.1
+                type user
+                type org
+                  relations
+                    define member: [user]
+                type group
+                  relations
+                    define parent as self
+                    define viewer as viewer from parent
+                """);
+    }
+
+    public void testEmptyDirectlyAssignableWithSpaceInvalid() throws Throwable {
+        doCodeTest("""
+                model
+                  schema 1.1
+                type user
+                type org
+                  relations
+                    define member: [ ]
+                    define reader: [user]
+                """);
+    }
+
+//    public void testInlineComments() throws Throwable {
+//        doCodeTest("""
+//                 model
+//                   schema 1.1
+//                 type user # This is a comment that should be allowed
+//                   relations # This is a comment that should be allowed
+//                     define admin: [user] # This is a comment that should be allowed
+//                 """);
+//    }
 
 }
